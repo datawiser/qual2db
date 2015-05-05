51dbb51c76235f7a49e16912e5994ddc6dd89e13
Owen Selles <oaselles@gmail.com>
2015-05-05 14:53:00 -0400

    Create example_script.py

---
+++ example_script.py
@@ -0,0 +1,16 @@
+from qual2db import sqlInterface, qualInterface, survey
+
+## Uncomment this function to display all the surveys in the Qualtrics API account:
+##qualInterface.printSurveys()
+
+## Fill in the surveyid of the survey you want to update here:
+surveyid = ''
+
+## Uncommment this line of code to create a survey object of the survey id provided above:
+##s = survey(surveyid)
+
+## Uncomment this function to print details about the survey:
+##s.describe()
+
+## Uncomment this function to add the survey to MySQL/update new responses:
+##s.update_sql.py()
