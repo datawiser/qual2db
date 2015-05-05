92f42659920828da68f11a178cab9488196a1008
Owen Selles <oaselles@gmail.com>
2015-05-05 14:16:32 -0400

    Delete errors.py

--- errors.py
+++
@@ -1,9 +0,0 @@
-# This is where all the error objects are stored.
-
-class ConnectionError(Exception):
-    def __str__(self):
-        return 'Cannot connect to MySQL. Please check your configuration file.'
-
-class APICallError(Exception):
-    def __str__(self):
-        return 'Cannot connect to Qualtrics. Please check your configuration file.'
