635658e3285b34004bcac78e38d42a8b701d9149
Owen Selles <oaselles@gmail.com>
2015-05-05 14:53:37 -0400

    Update example_script.py

--- example_script.py
+++ example_script.py
@@ -1,10 +1,10 @@
 from qual2db import sqlInterface, qualInterface, survey
 
 ## Uncomment this function to display all the surveys in the Qualtrics API account:
-qualInterface.printSurveys()
+##qualInterface.printSurveys()
 
 ## Fill in the surveyid of the survey you want to update here:
-surveyid = ''
+##surveyid = ''
 
 ## Uncommment this line of code to create a survey object of the survey id provided above:
 ##s = survey(surveyid)
