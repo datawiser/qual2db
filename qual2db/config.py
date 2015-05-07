6416b3df355f2fe013d6d6c578fa661a5c5bef95
Owen Selles <oaselles@gmail.com>
2015-05-07 16:16:23 -0400

    Update config.py

--- qual2db/config.py
+++ qual2db/config.py
@@ -1,1 +1,26 @@
+##### Credential Parser ########
+################################
+
+# This script parses credentials from the config.ini file
+
+import os
+import ConfigParser
+
+package_directory = os.path.dirname(__file__)
+config_file = os.path.join(package_directory, 'config.ini')
+
+config = ConfigParser.ConfigParser()
+config.read(config_file)
+
+baseurl = config.get('Qualtrics Credentials','baseurl')
+User = config.get('Qualtrics Credentials','User')
+Password = config.get('Qualtrics Credentials','Password')
+Token = config.get('Qualtrics Credentials','Token')
+Version = config.get('Qualtrics Credentials','Version')
+
+sqlUser = config.get('MySQL Credentials','sqlUser')
+sqlPassword = config.get('MySQL Credentials','sqlPassword')
+sqlHost = config.get('MySQL Credentials','sqlHost')
+sqlDB = config.get('MySQL Credentials','sqlDB')
+
 
