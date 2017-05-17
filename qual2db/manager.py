49b5ad47adc0e6c0c68e04c95b8938df4d0604b9
Owen Selles <oaselles@gmail.com>
2017-05-17 14:20:57 -0500

    combining classes into one file

--- qual2db/manager.py
+++ qual2db/manager.py
@@ -1,55 +1,194 @@
-"""
-survey.py
-"""
-
+import inspect
 import sqlite3
+
+import json
+import time
+import zipfile
+import os
+
+import pandas as pd
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from .datamodel import Base
+from qual2db.datamodel import Base
+from qual2db import datamodel
 
-from .datamodel import Survey, Block, Question, SubQuestion, Choice
-from .datamodel import Respondent, Response, default_respondent_fields
+from qual2db.datamodel import Survey, Block, Question, SubQuestion, Choice
+from qual2db.datamodel import Respondent, Response, default_respondent_fields
+
+import requests
+
+from qual2db.credentials import qualtrics_credentials as q_creds
+from qual2db.credentials import download_directory
 
 Session = sessionmaker()
 
 
-class Database(object):
-    """Interface for working with sqlalchemy, sqlite3, and data classes"""
+class DatabaseInterface:
 
-    def __init__(self, name):
-        global Session
+    def __init__(self, path):
         global Base
 
         # add db extension if unspecified
-        if name[:-3] != '.db':
-            name += '.db'
+        if path[:-3] != '.db':
+            path += '.db'
 
-        self.name = name
+        Session = sessionmaker()
+        self.path = path
+
         self.engine = create_engine(
-            'sqlite+pysqlite:///' + self.name, module=sqlite3.dbapi2, echo=False)
+            'sqlite+pysqlite:///' + self.path, module=sqlite3.dbapi2, echo=False)
 
         Session.configure(bind=self.engine)
         self.session = Session()
-
         Base.metadata.create_all(self.engine)
 
-    def survey(self, id=None, name=None):
-        if id:
-            query = self.session.query(Survey).filter_by(id=id)
-            return query.first()
+        self.connection = sqlite3.connect(self.path)
 
+        self.archetypes = dict()
+        for name, obj in inspect.getmembers(datamodel):
+            if inspect.isclass(obj):
+                try:
+                    self.archetypes[obj.__tablename__] = obj
+                except:
+                    continue
+
+    def save(self, Objects):
+        """Saves objects to the database. Takes a list of a single object."""
+        if isinstance(Objects, list):
+            self.session.add_all(Objects)
         else:
-            return None
-
-    def save(self, Entities):
-        if isinstance(Entities, list):
-            self.session.add_all(Entities)
-        else:
-            self.session.add(Entities)
-
+            self.session.add(Objects)
         self.session.commit()
+
+    def bind_table(self, table_name):
+        """Binds a database table to a function for retriving a pandas dataframe view of the database"""
+        def get_table(interface=self, table=table_name, sql=None, obj=False):
+            if obj:
+                archetype = interface.archetypes[table_name]
+                cols = archetype.__table__.columns.keys()
+                data = interface.session.query(archetype).all()
+
+                df = pd.DataFrame([[getattr(i, j) for j in cols] + [i]
+                                   for i in data], columns=cols + ['obj'])
+            else:
+                df = pd.read_sql_table(table_name, self.engine)
+                if sql:
+                    df = df.query(sql)
+            return df
+
+        get_table.__name__ = table_name
+        return get_table
+
+
+class QualtricsInterface:
+    """Interfaces with qualtrics.com via the API."""
+
+    def api_request(self, call='surveys', method='GET', parms=None, export=False, debug=False):
+        """Makes a api request
+
+        Parameters
+        ----------
+        call : the api call
+        method : the api method
+        parms : parameters required for the call
+        export : signifies if this call is a data download/export
+        debug : if True prints out log
+        """
+
+        url = q_creds['baseurl'] + call
+
+        headers = {
+            'x-api-token': q_creds['token'],
+            'content-type': 'application/json'
+        }
+
+        if parms:
+            parms = json.dumps(parms)
+
+        response = requests.request(method, url, data=parms, headers=headers)
+
+        if export:
+            qid = call.replace('responseexports/', '').replace('/file', '')
+            path = download_directory + qid + '_data'
+
+            with open(path + '.zip', 'wb') as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    f.write(chunk)
+
+            zipfile.ZipFile(path + '.zip').extractall(path)
+            return path
+
+        try:
+            data = response.json()
+        except:
+            return response.text
+
+        return data['result']
+
+    def listSurveys(self, debug=False):
+        """Creates a list of surveys the API account has access to."""
+        data = self.api_request(call='surveys', debug=debug)
+
+        survey_list = []
+        for survey in data['elements']:
+            qid = survey['id']
+            name = survey['name']
+            active = survey['isActive']
+
+            survey_list.append([qid, name, active])
+
+        return survey_list
+
+    def getSurvey(self, qid, debug=False):
+        """Quickly creates a dictionary with basic details about a given survey."""
+        schema = self.api_request(call='surveys/' + qid, debug=debug)
+        return schema
+
+    def getData(self, qid, last_response=None, debug=False):
+        """Gets survey data."""
+
+        parms = {
+            "surveyId": qid,
+            "format": "json"
+        }
+
+        if last_response:
+            parms['lastResponseId'] = last_response
+
+        print('Downloading data.')
+        data = self.api_request(call='responseexports/',
+                                method='POST', parms=parms, debug=debug)
+        export_id = data['id']
+
+        complete = 0
+        while complete < 100:
+            progress = self.api_request(
+                call='responseexports/' + export_id, method='GET', debug=debug)
+            complete = progress['percentComplete']
+            print(complete)
+
+        download_call = 'responseexports/' + export_id + '/file'
+        download_path = self.api_request(
+            call=download_call, method='GET', export=True, debug=debug)
+
+        data_file = download_path + '\\' + os.listdir(download_path)[0]
+        data = open(data_file, 'r')
+
+        return json.load(data)['responses']
+
+
+class Manager(DatabaseInterface, QualtricsInterface):
+    """Interface for working with sqlalchemy, sqlite3, and data classes"""
+
+    def __init__(self, path):
+        DatabaseInterface.__init__(self, path)
+        QualtricsInterface.__init__(self)
+
+        for table in self.archetypes:
+            func = self.bind_table(table)
+            setattr(self, table, func)
 
 
 def data_mapper(instance, dictionary, skip_keys=['choices', 'subQuestions'], qid=None):
