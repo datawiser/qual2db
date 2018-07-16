10b3d8fa2adc16fd63e72782e655cdebf5efca33
Jose Abraham Mena <abraham.mena@outlook.es>
2018-07-16 15:41:21 -0400

    Auto stash before merge of "development" and "origin/development"
    config.ini refactoring

--- qual2db.py
+++ qual2db.py
@@ -1,8 +1,14 @@
 import cherrypy
 import os
 from qual2db.gui import Root
+import configparser
 
 DIR = os.path.dirname(os.path.realpath(__file__))
+package_directory = os.path.dirname(__file__)
+config_file = os.path.join(package_directory, 'qual2db', 'config.ini')
+
+configurations = configparser.ConfigParser()
+configurations.read(config_file)
 
 qual_creds = {
     'baseurl':configurations['Qualtrics_Credentials']['baseurl'],
