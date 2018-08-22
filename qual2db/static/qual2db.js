ff7edd1617ad639ab7da41d9d20865e76c74e1d9
Jose Abraham Mena <abraham.mena@outlook.es>
2018-08-22 10:27:46 -0400

    Added datatables and layout changes
    Added datatables functionality.
    Changed layout slightly.

--- qual2db/static/qual2db.js
+++ qual2db/static/qual2db.js
@@ -1,24 +1,15 @@
 $(document).ready(function () {
     $('#qualtrics-table').DataTable( {
-        "scrollY": "100vh",
+        "scrollY": "75vh",
         "scrollCollapse": false,
         "paging": false,
         "searching": true,
-
-        columDefs: [
-            {
-                targets: -1,
-                className: 'dt-body-nowrap',
-                className: 'dt-compact',
-                className: 'dt-cell-border',
-            }
-        ]
     } );
 });
 
 $(document).ready(function () {
     $('#exported-table').DataTable({
-        "scrollY": "76vh",
+        "scrollY": "75vh",
         "scrollCollapse": false,
         "paging": false,
         "searching":true,
